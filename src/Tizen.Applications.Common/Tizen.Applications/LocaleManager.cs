c02e1afc8f356a1118148d05e32e7adffb9c6fd2
Hyeon-Uk <rlagusdnr120@gmail.com>
2025-08-27 11:23:49 +0900

    Implement SetApplicationLocale Method
    Signed-off-by: Hyeon-Uk <rlagusdnr120@gmail.com>

--- src/Tizen.Applications.Common/Tizen.Applications/LocaleManager.cs
+++ src/Tizen.Applications.Common/Tizen.Applications/LocaleManager.cs
@@ -14,30 +14,118 @@
  * limitations under the License.
  */
 
+using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace Tizen.Applications
 {
     static class LocaleManager
     {
-        private static string LogTag = "Tizen.Applications";
-        private static string SupportedLocalesFilePath = "/usr/share/i18n/SUPPORTED";
-        private static HashSet<string> supportedLocales;
-
-        static LocaleManager() { }
+        private static readonly string LogTag = "Tizen.Applications";
+        private static readonly string SupportedLocalesFilePath = "/usr/share/i18n/SUPPORTED";
+        private static bool _fileExists = File.Exists(SupportedLocalesFilePath);
+        private static HashSet<string> _supportedLocales;
 
         public static void SetApplicationLocale(CultureInfo info)
-        { 
-                        
+        {
+            if (!_fileExists)
+            {
+                Log.Error(LogTag, $"{SupportedLocalesFilePath} not found");
+                return;
+            }
+
+            string convertedLocale = ConvertCultureInfo(info);
+
+            if (VerifySupportedLocale(convertedLocale))
+            {
+                Interop.AppCore.UpdateLanguage(convertedLocale);
+            }
+            else
+            {
+                Log.Error(LogTag, $"Unsupported Language : {info.Name}");
+            }
         }
 
-        public static CultureInfo GetApplicationLocale() {
+        public static CultureInfo GetApplicationLocale()
+        {
             return null;
         }
 
-        public static CultureInfo GetSystemLocale() {
+        public static CultureInfo GetSystemLocale()
+        {
             return null;
+        }
+
+        private static string ConvertCultureInfo(CultureInfo info)
+        {
+            return $"{info.Name}.{Encoding.UTF8.BodyName}";
+        }
+
+        private static bool VerifySupportedLocale(string locale)
+        {
+            if (_supportedLocales == null)
+            {
+                LoadSupportedLocales();
+            }
+
+            return _supportedLocales.Contains(locale);
+        }
+
+        /// <summary>
+        /// Parses a line from the SUPPORTED file and formats it into "{language}_{country}.{encoding}".
+        /// The input lineParts can be:
+        /// 1. ["{localeString}", "{encodingString}"] e.g., ["en_US", "UTF-8"]
+        /// 2. ["{localeString}"] e.g., ["ko_KR"] (encoding will default to UTF-8)
+        /// The localeString itself might contain an encoding, e.g., "ko_KR.UTF-8", which will be stripped.
+        /// </summary>
+        /// <param name="lineParts">Array of strings from splitting a line by space.</param>
+        /// <returns>Formatted locale string or null if parsing fails.</returns>
+        private static string ParseAndFormatLocaleLine(string[] lineParts)
+        {
+            if (lineParts == null || lineParts.Length == 0 || string.IsNullOrWhiteSpace(lineParts[0]))
+            {
+                return null;
+            }
+
+            string localeString = lineParts[0];
+            string encodingString;
+
+            if (lineParts.Length == 2)
+            {
+                encodingString = lineParts[1];
+            }
+            else // lineParts.Length == 1
+            {
+                encodingString = Encoding.UTF8.BodyName; // Default to UTF-8 if not specified on the line
+            }
+
+            // Strip any existing encoding from the localeString (e.g., "ko_KR.UTF-8" -> "ko_KR")
+            int dotIndex = localeString.IndexOf('.');
+            if (dotIndex != -1)
+            {
+                localeString = localeString[..dotIndex];
+            }
+
+            if (string.IsNullOrWhiteSpace(localeString) || string.IsNullOrWhiteSpace(encodingString))
+            {
+                return null;
+            }
+
+            return $"{localeString}.{encodingString}";
+        }
+
+        private static void LoadSupportedLocales()
+        {
+            _supportedLocales = File.ReadAllLines(SupportedLocalesFilePath)
+                .Where(line => !string.IsNullOrWhiteSpace(line))
+                .Select(line => line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)) // Split by space only
+                .Select(lineParts => ParseAndFormatLocaleLine(lineParts))
+                .Where(formattedLocale => formattedLocale != null)
+                .ToHashSet();
         }
     }
 }
